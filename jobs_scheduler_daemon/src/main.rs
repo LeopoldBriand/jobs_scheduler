253538c48eb5a174a14121d49216be9b3f7b3b48
LeopoldBriand <leopold.briand@laposte.net>
2024-03-21 15:15:01 +0100

    refactor(daemon): history, logs and jobs in local files

--- jobs_scheduler_daemon/src/main.rs
+++ jobs_scheduler_daemon/src/main.rs
@@ -1,8 +1,10 @@
 use chrono::Utc;
 use core::fmt;
+use directories::UserDirs;
 use shlex::Shlex;
 use std::fs::{self, OpenOptions};
 use std::io::prelude::*;
+use std::path::Path;
 use std::process::Command;
 use std::time::Duration;
 use tokio_cron_scheduler::{Job, JobScheduler, JobSchedulerError};
@@ -10,13 +12,50 @@ use utils::parse_jobs;
 
 #[tokio::main]
 async fn main() -> Result<(), JobSchedulerError> {
-    let jobs_content =
-        fs::read_to_string("./jobs").expect("Should have been able to read the jobs file");
-    let jobs = parse_jobs(jobs_content);
+    let js_dir = UserDirs::new().unwrap().home_dir().join("job_scheduler");
+    let log_file = js_dir.join("logs");
+    let history_file = js_dir.join("history");
+    let jobs_file = js_dir.join("jobs");
+    // Check if files exists else create
+    if !js_dir.join("logs").exists() {
+        fs::create_dir_all(js_dir).expect("Not allowed to create ~/job_scheduler folder");
+        std::fs::File::create(log_file).expect("Not allowed to create ~/job_scheduler/logs file");
+    } else {
+        std::fs::File::create(log_file).expect("Not allowed to empty ~/job_scheduler/logs file");
+    }
+    if !history_file.exists() {
+        std::fs::File::create(history_file.clone())
+            .expect("Not allowed to create ~/job_scheduler/history file");
+    }
+    let jobs = match jobs_file.exists() {
+        true => match fs::read_to_string(jobs_file) {
+            Ok(jobs_content) => parse_jobs(jobs_content),
+            Err(_) => {
+                add_to_log(
+                    LogType::ERROR,
+                    format!("Should have been able to read the jobs file"),
+                );
+                vec![]
+            }
+        },
+        false => {
+            add_to_log(
+                LogType::DEBUG,
+                format!("No job file found in ~/job_scheduler/"),
+            );
+            vec![]
+        }
+    };
+
     let scheduler = JobScheduler::new().await?;
     for (name, cron, command) in jobs {
         scheduler
             .add(Job::new(cron.as_str(), move |_uuid, _l| {
+                let history_file = UserDirs::new()
+                    .unwrap()
+                    .home_dir()
+                    .join("job_scheduler")
+                    .join("history");
                 let timestamp = Utc::now().timestamp();
                 let mut lex = Shlex::new(&command);
                 let mut args = lex.by_ref().collect::<Vec<String>>();
@@ -28,6 +67,7 @@ async fn main() -> Result<(), JobSchedulerError> {
                                 timestamp,
                                 "SUCCESS",
                                 std::str::from_utf8(&output.stderr).unwrap(),
+                                &history_file,
                             )
                         } else {
                             add_to_history(
@@ -35,6 +75,7 @@ async fn main() -> Result<(), JobSchedulerError> {
                                 timestamp,
                                 "ERROR",
                                 std::str::from_utf8(&output.stderr).unwrap(),
+                                &history_file,
                             )
                         }
                         add_to_log(LogType::DEBUG, format!("Process {} executed", name))
@@ -53,19 +94,28 @@ async fn main() -> Result<(), JobSchedulerError> {
     }
 }
 
-fn add_to_history(name: String, timestamp: i64, status: &str, error_message: &str) {
+fn add_to_history(
+    name: String,
+    timestamp: i64,
+    status: &str,
+    error_message: &str,
+    history_file: &Path,
+) {
     let line = name + "," + &timestamp.to_string() + "," + status + "," + error_message;
     match OpenOptions::new()
         .write(true)
         .append(true)
-        .open("./history")
+        .open(history_file)
     {
         Ok(mut file) => {
             if let Err(e) = writeln!(file, "{}", line) {
-                add_to_log(LogType::ERROR, format!("Couldn't write to file: {}", e));
+                add_to_log(
+                    LogType::ERROR,
+                    format!("Couldn't write to history file: {}", e),
+                );
             }
         }
-        Err(_) => add_to_log(LogType::ERROR, format!("Couldn't open file:")),
+        Err(_) => add_to_log(LogType::ERROR, format!("Couldn't open history file:")),
     }
 }
 
@@ -84,6 +134,18 @@ impl fmt::Display for LogType {
 }
 
 fn add_to_log(log_type: LogType, text: String) {
-    // TODO: write to log file
+    let log_file = UserDirs::new()
+        .unwrap()
+        .home_dir()
+        .join("job_scheduler")
+        .join("logs");
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(log_file)
+        .unwrap();
+    if let Err(e) = writeln!(file, "{}: {}", log_type, text) {
+        println!("Error while writing to log file: {}", e);
+    }
     println!("{}: {}", log_type, text);
 }
