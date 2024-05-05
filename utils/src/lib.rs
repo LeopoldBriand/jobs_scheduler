3aea0aeedf8eb87aaf03aa0fef1f7f6d447b1aa9
LeopoldBriand <leopold.briand@laposte.net>
2024-05-05 23:10:50 +0200

    refactor: add get_next_date method from cron text

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,4 +1,4 @@
-use chrono::{DateTime, TimeZone, Utc};
+use chrono::{DateTime, Datelike, Duration, TimeZone, Timelike, Utc};
 use regex::Regex;
 
 mod parser;
@@ -9,6 +9,8 @@ const JOB_REGEX: &str = concat!(
     r"(?:(?:(?:\d+,)+\d+|(?:(?:\d+|\*)(?:\/|-)(?:\d+|\*))|\d+|\*) ?){5}", // cron
     ")[[:blank:]]+(.+)$"                         // command
 );
+
+const LOOP_LIMIT: u32 = 128;
 
 pub type History = Vec<HistoryStatement>;
 
@@ -83,6 +85,84 @@ pub fn parse_job(content: String) -> Option<Job> {
 }
 
 pub fn get_next_run(cron: &str) -> DateTime<Utc> {
-    parser::parse(cron);
-    todo!();
+    get_next_run_from(Utc::now(), cron)
+}
+
+pub fn get_next_run_from(start: DateTime<Utc>, cron: &str) -> DateTime<Utc> {
+    let fields = parser::parse(cron);
+    let mut current_date = start.clone();
+    let mut step_count: u32 = 0;
+    let days_in_month = vec![31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    while step_count < LOOP_LIMIT {
+        step_count += 1;
+
+        // Match day of month and day of week
+        let is_match_day_of_month = match_schedule(current_date.day(), &fields[2]);
+        let is_match_day_of_week =
+            match_schedule((current_date.weekday() as u8) as u32, &fields[4]);
+        let is_day_of_month_wildcard =
+            fields[2].len() >= days_in_month[current_date.month() as usize];
+        let is_day_of_week_wildcard = fields[4].len() == 7;
+        if (!is_match_day_of_month && (!is_match_day_of_week || is_day_of_week_wildcard))
+            || (!is_day_of_month_wildcard && is_day_of_week_wildcard && !is_match_day_of_month)
+            || (is_day_of_month_wildcard && !is_day_of_week_wildcard && !is_match_day_of_week)
+        {
+            current_date += Duration::days(1);
+            continue;
+        }
+
+        // Match month
+        if !match_schedule(current_date.month(), &fields[3]) {
+            let (year, month) = (current_date.year(), current_date.month());
+            let (next_year, next_month) = if month == 12 {
+                (year + 1, 1)
+            } else {
+                (year, month + 1)
+            };
+            current_date = Utc
+                .with_ymd_and_hms(next_year, next_month, 1, 0, 0, 0)
+                .unwrap();
+            continue;
+        }
+
+        // Match hour
+        if !match_schedule(current_date.hour(), &fields[1]) {
+            current_date += Duration::hours(1);
+            continue;
+        }
+
+        // Match minute
+        if !match_schedule(current_date.minute(), &fields[0]) {
+            current_date += Duration::minutes(1);
+            continue;
+        }
+        break;
+    }
+    if step_count > LOOP_LIMIT {
+        todo!("error")
+    }
+    return current_date;
+}
+
+fn match_schedule(value: u32, seq: &Vec<u32>) -> bool {
+    for el in seq {
+        if el >= &value {
+            return el == &value;
+        }
+    }
+    return seq[0] == value;
+}
+
+#[cfg(test)]
+mod tests {
+    use chrono::{TimeZone, Utc};
+
+    use crate::get_next_run_from;
+
+    #[test]
+    fn simple_cron() {
+        let start = Utc.with_ymd_and_hms(2024, 05, 1, 0, 0, 0).unwrap();
+        let date = get_next_run_from(start, "5 4 * * 6");
+        assert_eq!(date.to_string(), "2024-05-05 04:05:00 UTC");
+    }
 }
